e5922435562b631a35d96f8d7abed0f5e19bf709
gabjetté <gabjet.web@gmail.com>
2017-02-21 16:58:58 -0500

    Initialisation de l'exercice

---
+++ 02_objetLiteral.js
@@ -0,0 +1,21 @@
+var oProvince = {
+	"QC" : "Québec",
+	"ON" : "Ontario",
+	"AL" : "Alberta"
+}
+
+oProvince['NB'] = "Nouveau-Brunswick"
+oProvince.MA = "Manitoba"
+
+var monObjet = oProvince
+
+function affiche_objet(monObjet)
+{
+	for(propriete in monObjet)
+	{
+
+		console.log(propriete + ' - ' + monObjet[propriete])
+	}
+}
+
+affiche_objet(oProvince)
