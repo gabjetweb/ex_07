bce77444398d6a6401b224f55731b7d0aa064502
gabjetté <gabjet.web@gmail.com>
2017-02-21 17:48:30 -0500

    Ajustement et tests

--- 03_serveurNode.js
+++ 03_serveurNode.js
@@ -1,7 +1,22 @@
 var http = require("http");
+var fs = require("fs");
+var oProvince = fs.readFileSync("provinces.JSON");
+
+
+var monObjet = oProvince
+
+function affiche_objet(monObjet)
+{
+	for(propriete in monObjet)
+	{
+		console.log(propriete + ' - ' + monObjet[propriete])
+	}
+}
+
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write("<h1>Exercice_07</h1>");
+  response.write(affiche_objet(oProvince));
   response.end();
 }).listen(3000);
